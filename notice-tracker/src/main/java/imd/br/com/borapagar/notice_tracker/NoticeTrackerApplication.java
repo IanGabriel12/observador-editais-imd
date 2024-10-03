042f18e0ddf6c0adfb3a4901081bb3e297203860
Ian Gabriel <ianeigor12@gmail.com>
2024-10-03 19:43:09 -0300

    chore: initializing backend project

---
+++ notice-tracker/src/main/java/imd/br/com/borapagar/notice_tracker/NoticeTrackerApplication.java
@@ -0,0 +1,13 @@
+package imd.br.com.borapagar.notice_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeTrackerApplication.class, args);
+	}
+
+}
---
+++ notice-tracker/src/test/java/imd/br/com/borapagar/notice_tracker/NoticeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package imd.br.com.borapagar.notice_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
